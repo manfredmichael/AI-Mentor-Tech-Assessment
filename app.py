065de9828ee7bf0224331d2995092e8b2c444b13
manfredmichael <koskosanrider@gmail.com>
2024-03-04 14:40:52 +0700

    Add cashback page

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import time
 import streamlit as st
 
-import pandas as pd 
-from pipeline import get_recommendation
+import pandas as pd
+from pipeline import get_product_recommendation, get_user_recommendation
+
+
 
 purchase_history = pd.read_csv('model-training/purchase_history.csv')
 customer_interactions = pd.read_csv('model-training/customer_interactions.csv')
@@ -19,7 +21,9 @@ unique_products = list(data.product_id.unique())
 unique_users = list(data.user_id.unique())
 unique_category = list(data.category_id.unique())
 
+
 def recommendation_system():
+    st.title("TerraStore - Recommendation System Demo")
 
     user_id = st.selectbox(
         "Which user id would you want to see?",
@@ -29,12 +33,12 @@ def recommendation_system():
     )
 
     # Session details
-    page_views = st.number_input('Insert a page views (click amount)', min_value=0, step=1)
+    page_views = st.number_input('How many pages has the user visited in this session?', min_value=0, step=1)
 
-    session_time = st.number_input('Insert a session time in seconds', min_value=0, step=1)
+    session_time = st.number_input('How long has the user session last?', min_value=0, step=1)
 
     if user_id is not None:
-        recommendations = get_recommendation(
+        recommendations = get_product_recommendation(
             user_id=user_id, 
             page_views=page_views, 
             session_time=session_time
@@ -50,7 +54,7 @@ def recommendation_system():
                 "Product ID",
                 format="%d",
             )
-        },)
+        })
 
         st.write("#### Top 10 categories for user {}".format(user_id))
         st.dataframe(
@@ -58,11 +62,61 @@ def recommendation_system():
             hide_index=True)
         
 
+def cashback_and_discount_recommendation():
+    st.title("TerraStore - Which users deserve discounts?")
+
+    st.write("This page helps you to find which users are more likely to buy a given products. You could also tune the prices of the products to see how users might react.")
+
+    product_id = st.selectbox(
+        "Which product id would you want to see?",
+        unique_products,
+        index=None,
+        placeholder="Select product id",
+    )
+
+    # Session details
+    if product_id:
+        price_value = product_details[product_details.product_id==product_id].price.iloc[0]
+        category_id_value = product_details[product_details.product_id==product_id].category_id.iloc[0]
+        category_id_index = unique_category.index(category_id_value)
+    else: 
+        price_value = None
+        category_id_value = None
+    price = st.number_input('How much does it cost?', value=price_value, min_value=0.0)
+
+    category_id = st.selectbox(
+        "In which category does this product belong?",
+        unique_category,
+        index=category_id_index,
+        placeholder="Select category id",
+    )
+    
+
+    if product_id is not None:
+        recommendations = get_user_recommendation(
+            product_id=product_id, 
+            price=price, 
+            category_id=int(category_id)
+        )
+        # recommendations['category_id'] = recommendations['category_id'].astype(str)
+
+        st.write("#### Top 20 users who might buy {}".format(product_id))
+        st.dataframe(
+            recommendations.head(20),
+            hide_index=True,
+            column_config={
+            "user_id": st.column_config.NumberColumn(
+                "User ID",
+                format="%d",
+            )
+        })
+
 
 def main():
     page_names_to_funcs = {
         # "From Uploaded Picture": from_picture,
-        "Recommendation system": recommendation_system
+        "Recommendation system": recommendation_system,
+        "Cashback & Discount": cashback_and_discount_recommendation
     }
 
     selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
@@ -72,6 +126,5 @@ if __name__ == "__main__":
     st.set_page_config(
         page_title="Recommendation  System - Technical Interview", page_icon=":pencil2:"
     )
-    st.title("Recommendation  System")
     # st.sidebar.subheader("Configuration")
     main()
--- pipeline.py
+++ pipeline.py
@@ -8,8 +8,14 @@ model = models.get_model()
 model.load_weights('model-training/final_model.h5')
 
 product_details = pd.read_csv('model-training/product_details.csv')
+customer_interactions = pd.read_csv('model-training/customer_interactions.csv')
+user_details = customer_interactions.loc[customer_interactions.user_id.drop_duplicates().index]
 
-def get_recommendation(user_id, page_views, session_time, products=product_details, model=model):
+def get_product_recommendation(user_id, page_views, session_time, products=product_details, model=model):
+    """
+    Find which products a given user might are likely to buy
+
+    """
     products = products.copy()
     user_ids = [user_id] * len(products)
     page_views = [page_views] * len(products)
@@ -29,8 +35,36 @@ def get_recommendation(user_id, page_views, session_time, products=product_detai
                      input_['price'].values.reshape(-1, 1),
                      input_['category_id']
                     ]).numpy().reshape(-1)
+
+
+
+def get_user_recommendation(product_id, price, category_id, users=user_details, model=model):
+    """
+    Find which users are likely to buy which product
     
-    products['purchase_proba'] = pd.Series(results, index=products.index)
-    products = products.sort_values('purchase_proba', ascending=False)
+    """
+    users = users.copy()
+    product_ids = [product_id] * len(users)
+    prices = [price] * len(users)
+    category_ids = [category_id] * len(users)
+
     
-    return products
+    input_ = pd.DataFrame({'product_id': product_ids, 
+                           'user_id': list(users.user_id),
+                           'page_views': list(users.page_views),
+                           'session_time': list(users.session_time),
+                           'price': prices,
+                           'category_id': category_ids})
+    
+    results = model([input_['product_id'], 
+                     input_['user_id'],
+                     input_['page_views'].values.reshape(-1, 1),
+                     input_['session_time'].values.reshape(-1, 1),
+                     input_['price'].values.reshape(-1, 1),
+                     input_['category_id']
+                    ]).numpy().reshape(-1)
+    
+    users['purchase_proba'] = pd.Series(results, index=users.index)
+    users = users.sort_values('purchase_proba', ascending=False)
+    
+    return users
