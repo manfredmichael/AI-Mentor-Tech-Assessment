62f57391d48e038add91303fa675cf7b6466c03c
manfredmichael <koskosanrider@gmail.com>
2024-03-04 10:53:25 +0700

    Add streamlit app

---
+++ app.py
@@ -0,0 +1,62 @@
+import time
+import streamlit as st
+
+import pandas as pd 
+from pipeline import get_recommendation
+
+purchase_history = pd.read_csv('model-training/purchase_history.csv')
+customer_interactions = pd.read_csv('model-training/customer_interactions.csv')
+product_details = pd.read_csv('model-training/product_details.csv')
+
+data = purchase_history.merge(customer_interactions)
+data = data.merge(product_details)
+
+n_products = data.product_id.nunique()
+n_users = data.user_id.nunique()
+n_category =  data.category_id.nunique()
+
+unique_products = list(data.product_id.unique())
+unique_users = list(data.user_id.unique())
+unique_category = list(data.category_id.unique())
+
+def recommendation_system():
+
+    user_id = st.selectbox(
+        "Which user id would you want to see?",
+        unique_users,
+        index=None,
+        placeholder="Select user id",
+    )
+
+    # Session details
+    page_views = st.number_input('Insert a page views (click amount)', min_value=0, step=1)
+
+    session_time = st.number_input('Insert a session time in seconds', min_value=0, step=1)
+
+    if user_id is not None:
+        recommendations = get_recommendation(
+            user_id=user_id, 
+            page_views=page_views, 
+            session_time=session_time
+        )
+        recommendations = recommendations.head(10)
+        st.dataframe(recommendations)
+        
+
+
+def main():
+    page_names_to_funcs = {
+        # "From Uploaded Picture": from_picture,
+        "Recommendation system": recommendation_system
+    }
+
+    selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+    page_names_to_funcs[selected_page]()
+
+if __name__ == "__main__":
+    st.set_page_config(
+        page_title="Recommendation  System - Technical Interview", page_icon=":pencil2:"
+    )
+    st.title("Recommendation  System")
+    # st.sidebar.subheader("Configuration")
+    main()
